e1a9565de423bab1a28f096dfbba6096ac88671b
stuffi <st_tuff@me.com>
2021-10-04 17:56:11 +1100

    - Updated Routes in Web.php, removed auth test routes

--- app/Routes/Web.php
+++ app/Routes/Web.php
@@ -12,16 +12,18 @@ use app\providers\RouteServiceProvider;
 
 class Web
 {
-
     //**** WEB CLASS Contains all the route groups relating to the website ****\\
     public function __construct(RouteServiceProvider $routeServiceProvider){
 
+        //create core route group first
         $default = new RouteGroup();
+        //next add the routes we want to the group, firstly add the prefix, then the relevant controller
         $default->addRoute("/","DefaultController");
         $default->addRoute("/login","DefaultController");
         $default->addRoute("/signup","DefaultController");
         $default->addRoute("/account","DefaultController");
         $default->addRoute("/help","DefaultController");
+        //finally, register the route group with the core route service provider using the correct parent prefix for this group
         $routeServiceProvider->addRouteGroup("/",$default);
 
         $test = new RouteGroup();
@@ -29,13 +31,6 @@ class Web
         $test->addRoute("/meme","TestController");
         $test->addRoute("/jack","TestController");
         $routeServiceProvider->addRouteGroup("/test",$test);
-
-        $auth = new RouteGroup();
-        $auth->addRoute("/","AuthController");
-        $auth->addRoute("/login","AuthController");
-        $routeServiceProvider->addRouteGroup("/auth",$auth);
-
-
 
     }
 
