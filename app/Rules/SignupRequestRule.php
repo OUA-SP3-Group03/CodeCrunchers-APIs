46aa255d73e7f4675a6e2d6c55159d54000ac740
stuffi <st_tuff@me.com>
2021-10-04 20:10:44 +1100

    - Created SignupRequestRule.php and added all request required for successful signup

---
+++ app/Rules/SignupRequestRule.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Copyright Jack Harris
+ * Peninsula Interactive - CodeCrunchers
+ * Last Updated - 4/10/2021
+ */
+
+namespace app\rules;
+
+use app\core\Rule;
+
+//Extends the main Rule class
+class SignupRequestRule extends Rule
+{
+    //constructor method is called when the object is created, our rules must be in here
+    public function __construct()
+    {
+        //signup request rule, this is the rule that we use when validating our signup requests before storing the request in the database
+        $this->rules["email"] = ["required" => true, "unique" => true];
+        $this->rules["first_name"] = ["required" => true];
+        $this->rules["last_name"] = ["required" => true];
+        $this->rules["password"] = ["required" => true];
+    }
+
+}
