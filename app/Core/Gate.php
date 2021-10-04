3aa66cb45e85f760786545c72ec44bdc03f28873
stuffi <st_tuff@me.com>
2021-10-04 18:19:00 +1100

    - Added Gate class to provide static functions used to restrict the access of controllers and endpoints - Added Gate function to get error string from code

---
+++ app/Core/Gate.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Copyright Jack Harris
+ * Peninsula Interactive - CodeCrunchers
+ * Last Updated - 4/10/2021
+ */
+
+namespace app\core;
+
+class Gate
+{
+    //method function restricts specific controllers or sub controllers from being accessed by specific method types
+    public static function post(): ?int
+    {
+        if($_SERVER['REQUEST_METHOD'] == "POST"){
+            return null;
+        }
+        return 405;
+    }
+    public static function get(): ?int{
+        if($_SERVER['REQUEST_METHOD'] == "GET"){
+            return null;
+        }
+        return 405;
+    }
+
+    //Gate get error data from code
+    public static function getError($code): string
+    {
+        return match ($code) {
+            405 => "405: The request method is not supported for the requested resource",
+            404 => "404: The requested resource could not be found",
+            default => "invalid error code",
+        };
+    }
+
+}
--- app/Views/auth/login.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Copyright Jack Harris
- * Peninsula Interactive - CodeCrunchers
- * Last Updated - 4/10/2021
- */
-
-?>
-<h1>Auth / login</h1>
