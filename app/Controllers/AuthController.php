2d8d0f0b9ddb6604d4683a8dd734acabfdd9d3cc
stuffi <st_tuff@me.com>
2021-10-04 19:34:33 +1100

    - Updated AuthController.php to use new request pass through

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -9,18 +9,22 @@ namespace app\controllers;
 
 use app\core\Controller;
 use app\core\Gate;
+use app\core\Request;
 
 class AuthController extends Controller
 {
 
-    public function __construct($route = "/")
+    public function __construct(String $route = "/", Request $request = null)
     {
+        $this->request = $request;
+
         if(Gate::post()) {
             echo Gate::getError(Gate::post());
         }else {
             switch ($route) {
                 case "/login":
                     echo "Authentication Login end point **** ADD LATER ****";
+                    print_r($request->getPostData());
                     break;
                 case "/logout":
                     echo "Authentication Logout **** ADD LATER ****";
