ad44db27aeeb04acfa6084559904903e2d8da756
stuffi <st_tuff@me.com>
2021-10-05 12:22:44 +1100

    - Updated placeholder index page

--- app/Views/default/index.php
+++ app/Views/default/index.php
@@ -5,5 +5,12 @@
  * Last Updated - 4/10/2021
  */
 
+include VIEWS."header.php";
+include VIEWS."navbar.php";
 ?>
-<h1>Code Crunchers</h1>
+
+<div class="container">
+
+<h1>Welcome to our home page!</h1>
+<p>This page will be the main landing index page for our game and contain all the information and screen shots to grab visitors attention</p>
+</div>
