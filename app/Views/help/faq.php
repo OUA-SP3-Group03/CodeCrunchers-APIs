706c3edaff1c7383a62a053d76f82a28a2e63705
stuffi <st_tuff@me.com>
2021-10-05 12:37:24 +1100

    - Created placeholder faq page

---
+++ app/Views/help/faq.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Copyright Jack Harris
+ * Peninsula Interactive - CodeCrunchers
+ * Last Updated - 4/10/2021
+ */
+
+include VIEWS . "Header.php";
+include VIEWS . "Navbar.php";
+?>
+
+<div class="container">
+
+    <h1>Code Crunchers | Frequently Asked Questions</h1>
+    <p>Coming soon!</p>
+</div>
