d5f1a49aa32ff34bd07f24da5a849de8e0aa1fb0
stuffi <st_tuff@me.com>
2021-10-05 12:21:43 +1100

    - Created placeholder login page

--- app/Views/default/login.php
+++ app/Views/default/login.php
@@ -4,6 +4,21 @@
  * Peninsula Interactive - CodeCrunchers
  * Last Updated - 4/10/2021
  */
-
+include VIEWS."header.php";
+include VIEWS."navbar.php";
 ?>
-<h1>Login!</h1>
+<div class="container">
+<h1>Login</h1>
+<form style="max-width: 400px; justify-content: center">
+    <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    </div>
+    <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword1">
+    </div>
+    <p>Dont already have an account? <a href="/register">register for free today.</a></p>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+</div>
