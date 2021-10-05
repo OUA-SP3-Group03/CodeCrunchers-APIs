e1339650f3ea63ef2468e7d00440422342cfdd8c
stuffi <st_tuff@me.com>
2021-10-05 12:37:42 +1100

    - Created help controller to handle the /help/ route group

---
+++ app/Controllers/HelpController.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Copyright Jack Harris
+ * Peninsula Interactive - CodeCrunchers
+ * Last Updated - 4/10/2021
+ */
+
+namespace app\controllers;
+
+use app\core\Console;
+use app\core\Controller;
+use app\core\Gate;
+use app\core\Request;
+
+class HelpController extends Controller
+{
+
+    public function __construct($route = "/", Request $request = null)
+    {
+        $this->request = $request;
+        if (Gate::get()) {
+            echo Gate::getError(Gate::get());
+        } else {
+            switch ($route) {
+                case "/":
+                    include VIEWS . "help" . DIRECTORY_SEPARATOR . "index.php";
+                    break;
+                case "/tos":
+                    include VIEWS . "help" . DIRECTORY_SEPARATOR . "tos.php";
+                    break;
+                case "/faq":
+                    include VIEWS . "help" . DIRECTORY_SEPARATOR . "faq.php";
+                    break;
+            }
+        }
+    }
+
+
+}
