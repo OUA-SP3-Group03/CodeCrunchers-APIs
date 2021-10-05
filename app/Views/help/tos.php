a90941e6057e27c689796a898eced5c0daab1b6e
stuffi <st_tuff@me.com>
2021-10-05 12:38:44 +1100

    - Created placeholder Terms of Service Page

---
+++ app/Views/help/tos.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Copyright Jack Harris
+ * Peninsula Interactive - CodeCrunchers
+ * Last Updated - 4/10/2021
+ */
+
+include VIEWS . "Header.php";
+include VIEWS . "Navbar.php";
+?>
+
+<div class="container">
+
+    <h1>Code Crunchers | Terms of Service</h1>
+    <p>Coming soon!</p>
+</div>
