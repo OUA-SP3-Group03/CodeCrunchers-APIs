f470f9bd3ce532b24315907a7ae60ba381bee545
stuffi <st_tuff@me.com>
2021-10-05 12:41:03 +1100

    - Added TOS & FAQ links to help index page

--- app/Views/help/index.php
+++ app/Views/help/index.php
@@ -12,5 +12,12 @@ include VIEWS . "Navbar.php";
 <div class="container">
 
     <h1>Help & FAQ Page</h1>
+    <br>
     <p>Coming soon!</p>
+    <br>
+    <p><a href="/help/tos">Terms of Service</a></p>
+    <p><a href="/help/faq">Frequently Asked Questions</a></p>
+    <br>
+
+
 </div>
