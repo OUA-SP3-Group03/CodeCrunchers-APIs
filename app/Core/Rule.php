e8311a6ed858277a5ae9f1e3bb190f8fea89f927
stuffi <st_tuff@me.com>
2021-10-05 13:37:30 +1100

    - Updated Rule to store the database table name in a protected variable

--- app/Core/Rule.php
+++ app/Core/Rule.php
@@ -7,9 +7,13 @@
 
 namespace app\core;
 
-class Rule
+abstract class Rule
 {
     protected array $rules;
+    protected String $databaseTable;
+
+    //Abstract constructor for use by child rules
+    public abstract function __construct();
 
     // PHP cannot return an array using the __toString method so json must be returned and then converted into an array
     public function __toString(): string
