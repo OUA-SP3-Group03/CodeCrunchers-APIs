021bb7cdde56a4c28fbae803a63d21b3c3685af9
stuffi <st_tuff@me.com>
2021-10-05 12:21:13 +1100

    - Created Placeholder scores page

---
+++ app/Views/default/scores.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Copyright Jack Harris
+ * Peninsula Interactive - CodeCrunchers
+ * Last Updated - 4/10/2021
+ */
+
+include VIEWS."header.php";
+include VIEWS."navbar.php";
+?>
+
+<div class="container">
+
+    <h1>Global High Scores</h1>
+    <p>page coming soon</p>
+</div>
