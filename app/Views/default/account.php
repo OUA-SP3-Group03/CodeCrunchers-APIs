ae24bd9202d79e1755d601ab1c0d5015338343c1
Jack Harris <79836947+s3893749@users.noreply.github.com>
2021-10-06 20:28:14 +1100

    - Created my account placeholder page accessible by /account, protected by Gate::get and Gate::loggedIn

---
+++ app/Views/default/account.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Copyright Jack Harris
+ * Peninsula Interactive - CodeCrunchers
+ * Last Updated - 4/10/2021
+ */
+
+include VIEWS."header.php";
+include VIEWS."navbar.php";
+?>
+
+<div class="container">
+
+    <h1>My Account</h1>
+    <p>TODO: add my account page, this page will house all the my account information for the user and allow them to change information and view login history</p>
+
+
+
+</div>
