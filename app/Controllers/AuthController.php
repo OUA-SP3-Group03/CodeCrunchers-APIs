b8669f5cfb443669ad1bbe64ec7df387ebd52494
Jack Harris <79836947+s3893749@users.noreply.github.com>
2021-10-06 18:47:46 +1100

    - Update Auth controller to include testing code for the login calls

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -13,6 +13,8 @@ use app\core\Gate;
 use app\core\Request;
 use app\rules\LoginRequestRule;
 use app\rules\SignupRequestRule;
+use app\services\Cookie;
+use app\services\Token;
 use app\services\User;
 
 class AuthController extends Controller
@@ -27,16 +29,38 @@ class AuthController extends Controller
         }else {
             switch ($route) {
                 case "/login":
-                    //echo "Authentication Login end point **** ADD LATER **** \n";
+                    //validate request with login request rules
                     $this->request->validate(new LoginRequestRule());
+                    //check for any requests, if there are requests console log them
                     if($this->request->getErrors() != []) {
                         Console::log(json_encode($this->request->getErrors()));
                     }else{
+                        //now we get the result of the user login and pass it the request
                         $result = User::login($this->request);
+                        //if the result is not null we can see the login has worked, a failed login will return null
                         if($result != null){
-                            echo "Welcome ".$result[User::$first_name]." ".$result[User::$last_name];
+                            //now we start our try catch loop for token creation
+                            try {
+                                //we set the token response to a new token and pass it the user id from the valid login as well as the token type from the request, this is either "web" or "game"
+                                $token_response = Token::create($this->request->getPostData()["type"], $result[User::$user_id]);
+
+                                //now we process the return for a web type
+                                if($request->getPostData()["type"] == "web"){
+                                    //echo true for success
+                                    echo true;
+                                    //create the cookie for the web
+                                    Cookie::create("codecrunchers",$token_response["token"],7);
+                                }else{
+                                    //else we assume its the game type and return the token as json for the game to save
+                                    echo json_encode($token_response);
+                                }
+                                //finally if we have any errors we print them to console
+                            } catch (\Exception $e) {Console::log($e->getMessage());}
+
+                        //else we have assumed that the core login failed and return false
                         }else{
-                            echo "Login failed, please check your email and password and try again";
+                            //echo false for failed login attempt
+                            echo false;
                         }
                     }
                     break;
@@ -60,6 +84,10 @@ class AuthController extends Controller
                        }
 
                     }
+                    break;
+                case "/get":
+
+                    break;
             }
         }
     }
