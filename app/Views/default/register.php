643887d80e66b9e8ce4edb94236dfef65a71c9df
Jack Harris <79836947+s3893749@users.noreply.github.com>
2021-11-16 14:41:09 +1100

    - Updated Registration page to use new stylesheet css classes

--- app/Views/default/register.php
+++ app/Views/default/register.php
@@ -9,62 +9,65 @@ include VIEWS . "header.php";
 include VIEWS . "navbar.php";
 ?>
 
-<div class="container">
+<div class="container"  id="body-content">
 
-    <h1>Registration page</h1>
-    <form style="max-width: 400px; justify-content: center">
+    <form style=" justify-content: center; max-width: 1200px" id="login-form" >
+        <h1>Registration</h1>
         <div class="alert alert-danger" role="alert" style="display: none" id="alert-registration-failed">
             <strong>Registration Failed!</strong> please check all the required information is entered correctly and try again
         </div>
-        <div class="mb-3">
-            <div class="alert alert-warning" role="alert" style="display: none" id="input-firstname-alert">
-                please enter your first name
+        <div class="flex-row d-flex">
+            <div class="mb-3 p-2"  style="width: 300px">
+                <div class="alert alert-warning" role="alert" style="display: none" id="input-firstname-alert">
+                    please enter your first name
+                </div>
+                <label for="first_name" class="form-label">First name</label>
+                <input type="text" class="form-control" id="first_name">
             </div>
-            <label for="first_name" class="form-label">First name</label>
-            <input type="text" class="form-control" id="first_name">
-        </div>
-        <div class="mb-3">
-            <div class="alert alert-warning" role="alert" style="display: none" id="input-lastname-alert">
-                please enter your last name
+            <div class="mb-3 p-2" style="width: 300px">
+                <div class="alert alert-warning" role="alert" style="display: none" id="input-lastname-alert">
+                    please enter your last name
+                </div>
+                <label for="last_name" class="form-label" >Last name</label>
+                <input type="text" class="form-control" id="last_name">
             </div>
-            <label for="last_name" class="form-label">Last name</label>
-            <input type="text" class="form-control" id="last_name">
         </div>
-        <div class="mb-3">
-            <div class="alert alert-warning" role="alert" style="display: none" id="input-username-alert">
-                please enter a valid username, usernames cannot match and must be unique!
+        <div class="flex-row d-flex">
+            <div class="mb-3 p-2" style="width: 300px">
+                <div class="alert alert-warning" role="alert" style="display: none" id="input-username-alert">
+                    please enter a valid username, usernames cannot match and must be unique!
+                </div>
+                <label for="username" class="form-label">Username</label>
+                <input type="text" class="form-control" id="username"  style="max-width: 300px">
             </div>
-            <label for="username" class="form-label">Username</label>
-            <input type="text" class="form-control" id="username">
+            <div class="mb-3 p-2" style="width: 300px">
+                <div class="alert alert-warning" role="alert" style="display: none" id="input-email-alert">
+                    please enter a valid email address, emails cannot match and must be unique!
+                </div>
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="email"  style="max-width: 300px">
+            </div>
         </div>
-    <div class="mb-3">
-        <div class="alert alert-warning" role="alert" style="display: none" id="input-email-alert">
-            please enter a valid email address, emails cannot match and must be unique!
-        </div>
-        <label for="exampleInputEmail1" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="email">
-    </div>
-    <div class="mb-3">
         <div class="alert alert-warning" role="alert" style="display: none" id="input-password1-alert">
             please check your password is not blank and that both password fields match!
         </div>
-        <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input type="password" class="form-control" id="password1">
-    </div>
-        <div class="mb-3">
-            <div class="alert alert-warning" role="alert" style="display: none" id="input-password2-alert">
-                please check your password is not blank and that both password fields match!
+        <div class="flex-row d-flex">
+            <div class="mb-3 p-2" style="width: 300px">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password1"  style="max-width: 300px">
             </div>
-            <label for="exampleInputPassword2" class="form-label">Confirm Password</label>
-            <input type="password" class="form-control" id="password2">
+            <div class="mb-3 p-2" style="width: 300px">
+                <label for="exampleInputPassword2" class="form-label">Confirm Password</label>
+                <input type="password" class="form-control" id="password2"  style="max-width: 300px">
+            </div>
         </div>
-        <div class="mb-3 form-check">
-            <div class="alert alert-warning" role="alert" style="display: none" id="input-tos-alert">
+        <div class="alert alert-warning" role="alert" style="display: none" id="input-tos-alert">
             Please read and accept our terms of service before creating an account
         </div>
-        <input type="checkbox" class="form-check-input" id="accept-tos">
-        <label class="form-check-label" for="exampleCheck1">I Agree to the Code Crunchers <a href="/help/tos">Terms of Service</a></label>
-</div>
+            <div class="mb-3 form-check">
+                <input type="checkbox" class="form-check-input" id="accept-tos">
+                <label class="form-check-label" for="exampleCheck1">I Agree to the Code Crunchers <a href="/help/tos">Terms of Service</a></label>
+            </div>
     <p>Already have an account? <a href="/login">login now!</a></p>
     <button onclick="register()" type="button" class="btn btn-primary" id="registerButton">Submit</button>
     </form>
