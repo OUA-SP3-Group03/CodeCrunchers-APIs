42754a546ad087f3a732a52bb4c8673a40aa4a7c
Jack Harris <79836947+s3893749@users.noreply.github.com>
2021-11-16 14:23:42 +1100

    - Updated Account Page to show account info and delete button. - TODO delete button functionality still required, currently its a dummy button.

--- app/Views/default/account.php
+++ app/Views/default/account.php
@@ -5,11 +5,35 @@
  * Last Updated - 4/10/2021
  */
 
+use app\services\UserService;
+
 include VIEWS."header.php";
 include VIEWS."navbar.php";
 ?>
 
 <div class="container">
     <h1>My Account</h1>
-    <p>TODO: add my account page, this page will house all the my account information for the user and allow them to change information and view login history</p>
+    <br>
+    <p style="color: white"><strong>My Details</strong></p>
+
+    <?php
+
+    $userdata = UserService::getInfoWeb($_COOKIE["codecrunchers"]);
+    echo '<p style="color: white">';
+    echo "Username: ".$userdata[1]."</p>";
+    echo '<p style="color: white">';
+    echo "Name: ".$userdata[2]." ".$userdata[3]."</p>";
+    echo '<p style="color: white">';
+    echo "Email: ".$userdata[4]."</p>";
+    echo '<p style="color: white">';
+    echo "Registration Date: ".date("d F Y H:i:s", $userdata[6])."</p>";
+    ?>
+    <br>
+    <button onclick="deleteAccount()" type="button" class="btn btn-primary" id="login-button" style="background-color: red">Delete Account</button>
 </div>
+
+<script>
+    function deleteAccount(){
+
+    }
+</script>
