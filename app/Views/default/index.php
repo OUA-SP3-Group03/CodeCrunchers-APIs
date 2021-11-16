95772a65cd1ea494737c48ed633e4065efbf9bb7
Jack Harris <79836947+s3893749@users.noreply.github.com>
2021-11-16 14:39:01 +1100

    - Added index page description and screenshot

--- app/Views/default/index.php
+++ app/Views/default/index.php
@@ -11,7 +11,28 @@ include VIEWS."navbar.php";
 
 <div class="container">
 
-<h1>Welcome to our home page!</h1>
-<p>This page will be the main landing index page for our game and contain all the information and screen shots to grab visitors attention</p>
+<h1>Welcome to Code Crunchers!</h1>
+<p style="color: white"> In Code Crunchers, you play as a first year programming
+    student with no prior coding experience who must progress through 3 stages of
+    programming. But this is no ordinary programming course! You will need to navigate
+    through increasingly tricky levels and defeat enemies using your coding weapons, but
+    beware – the dreaded Master of Bad Coding lies in wait to challenge any would-be
+    programmers! Collect power ups by defeating enemies to help you progress and
+    defeat the Master, or try to complete the game as quickly as possible to earn a high
+    score!</p>
+
+    <p style="color: white">Code Crunchers functions as a side-scrolling action game: the player needs to
+        progress through a series of increasingly difficult levels, each populated with a different
+        layouts and selection of enemies, with the aim of reaching and defeating the final boss
+        at the final game level. The player has the ability to move left and right, jump over
+        obstacles or between platforms, and shoot their weapon to defeat enemies. Enemies
+        may drop ‘power ups’ which will improve the player’s weapon damage or movement
+        and help them progress through the game. The game will record the player’s score,
+        and allow them to save their scores for sharing.</p>
+    <br>
+    <img src="/api/public/alpha-v2-screenshot" alt="Code Crunchers Public Alpha Screenshot" class="center">
+    <p style="text-align: center; color: white">Code Crunchers Alpha v0.1</p>
+
+
 
 </div>
