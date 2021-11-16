e342997215f1c3e86f0148f1b3a71623edc1f004
Jack Harris <79836947+s3893749@users.noreply.github.com>
2021-11-16 16:47:51 +1100

    - Updated scores.php view page to show the top 50 views.

--- app/Views/default/scores.php
+++ app/Views/default/scores.php
@@ -5,6 +5,8 @@
  * Last Updated - 4/10/2021
  */
 
+use app\services\ScoreService;
+
 include VIEWS."header.php";
 include VIEWS."navbar.php";
 ?>
@@ -12,5 +14,14 @@ include VIEWS."navbar.php";
 <div class="container">
 
     <h1>Global High Scores</h1>
-    <p style="color: white">High Scores List Coming Soon!</p>
+    <p style="color: white; text-align: center">Top 50 High Scores</p>
+
+    <?php
+
+    $scores = ScoreService::getScores();
+
+    foreach ($scores as $score){
+            echo '<p style="color: white; text-align: center"><strong>'.$score["score"]."</strong> by  ".$score["username"]." on ".date("d F Y H:i:s",$score["date"]).'</p>';
+    }
+    ?>
 </div>
