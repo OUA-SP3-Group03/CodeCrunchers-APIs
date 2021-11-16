8924b799afff93657d4600a465dd8b042a184bbe
Jack Harris <79836947+s3893749@users.noreply.github.com>
2021-11-16 14:35:08 +1100

    - Created new download view

---
+++ app/Views/default/download.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Copyright Jack Harris
+ * Peninsula Interactive - CodeCrunchers
+ * Last Updated - 4/10/2021
+ */
+
+include VIEWS."header.php";
+include VIEWS."navbar.php";
+?>
+
+<div class="container">
+
+    <h1>Downloads</h1>
+    <p style="color: white"> <strong>Game</strong></p>
+    <p style="color: white">Alpha v2: <a href="#">Coming soon</a></p>
+    <br>
+    <p style="color: white"> <strong>Source Code</strong></p>
+    <p style="color: white">Game & Engine (Java 15+): <a href="https://github.com/OUA-SP3-Group03/CodeCrunchers-Game">https://github.com/OUA-SP3-Group03/CodeCrunchers-Game</a></p>
+    <p style="color: white">Web & API (PHP 8.0+): <a href="https://github.com/OUA-SP3-Group03/CodeCrunchers-APIs">https://github.com/OUA-SP3-Group03/CodeCrunchers-APIs</a></p>
+    <p style="color: white">
+        <br>
+        <strong>License</strong><br>
+        Copyright 2021 Jack Harris, Bruno Fonseca, Jaejun Oh, Alex Vorraro, Sean Atherton, Chris Duncan
+        Licensed under the Apache License, Version 2.0 (the "License");
+        you may not use this file except in compliance with the License.
+        You may obtain a copy of the License at
+        <br>
+        <br>
+        <a href="http://www.apache.org/licenses/LICENSE-2.0">http://www.apache.org/licenses/LICENSE-2.0</a>
+        <br>
+        <br>
+        Unless required by applicable law or agreed to in writing, software
+        distributed under the License is distributed on an "AS IS" BASIS,
+        WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+        See the License for the specific language governing permissions and
+        limitations under the License.</p>
+
+</div>
