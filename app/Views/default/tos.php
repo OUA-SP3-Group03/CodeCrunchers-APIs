388e8c19ad03e683156708d6fab46d3d896746c3
Jack Harris <79836947+s3893749@users.noreply.github.com>
2021-11-21 13:53:05 +1100

    - Created new TOS page and upload TOS content

---
+++ app/Views/default/tos.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Copyright Jack Harris
+ * Peninsula Interactive - CodeCrunchers
+ * Last Updated - 4/10/2021
+ */
+
+include VIEWS."Header.php";
+include VIEWS."Navbar.php";
+?>
+
+<div class="container">
+
+    <h1>Terms of Service</h1>
+    <p style="color: white"> Please read these terms of service carefully before using https://codecrunchers.io/ (our “Website”) or the Code Crunchers game (our “Game”), operated by RMIT-SP3-GROUP03. </p>
+    <br>
+    <p style="color: white"><strong>CONDITIONS OF USE</strong></p>
+    <p style="color: white">By visiting our Website or our Game, you fully agree to these Terms Of Service. If you do not agree with these Terms of Service, you should not use our Website or our Game. </p>
+
+    <br>
+    <p style="color: white"><strong>COPYRIGHT</strong></p>
+    <p style="color: white">Our Website, Game, and all content published may not be copied, reproduced, republished, uploaded, posted, publicly displayed, distributed, sold, license, or otherwise exploited for any commercial purpose whatsoever.  </p>
+
+    <br>
+    <p style="color: white"><strong>APPLICABLE LAW </strong></p>
+    <p style="color: white">Subject to applicable law, when you use our Website or Game, you use them as is. This means we do not provide any guarantees or promises about the standard or quality of our Website, our Game, its content and services. We are not responsible for any losses you may suffer as a result of using our products. </p>
+
+    <br>
+    <p style="color: white"><strong>USER ACCOUNT  </strong></p>
+    <p style="color: white">You may need to register a Code Crunchers account with us to access all features of our Website or our Game. Any details that you use to create an account, and all activities occurring on your account, are solely your responsibility. We do not guarantee the security and privacy of your personal data. By creating an account, you are aware of the risks concerning your personal data and release RMIT-SP3-GROUP03 from all account or personal data claims. </p>
+
+    <br>
+    <p style="color: white">Last Updated 21/11/2021</p>
+
+
+
+</div>
